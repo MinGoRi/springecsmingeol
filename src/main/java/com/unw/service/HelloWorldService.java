5c5e302a16d9c61c08208ee4d315641954d34bb4
user <mingeol@mz.co.kr>
2020-02-28 16:17:16 +0900

    1.3 xptmxm

--- src/main/java/com/unw/service/HelloWorldService.java
+++ src/main/java/com/unw/service/HelloWorldService.java
@@ -16,7 +16,7 @@ public class HelloWorldService {
     @GET
     @Path("/hello")
     public Response test() {
-        return Response.status(200).entity("Rest Enpoint , Date 02.28.2020 - HELLO AWS Friends - V1.2 mingeol test").build();
+        return Response.status(200).entity("Rest Enpoint , Date 02.28.2020 - HELLO AWS Friends - V1.3 mingeol test").build();
     }
 
 }
